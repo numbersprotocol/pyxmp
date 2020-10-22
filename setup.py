20ae1ac84dd78df0bb7299adb1ff975e1b4a48e1
James Chien <shc261392@gmail.com>
2020-10-22 16:29:35 +0800

    Load README.md for package long description
    Signed-off-by: James Chien <shc261392@gmail.com>

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='pyxmp',
     version='0.1.0',
     description='A Python XMP metadata injection tool based on py3exiv2.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/numbersprotocol/pyxmp',
     author='Numbersprotocol',
     author_email='hi@numbersprotocol.io',
