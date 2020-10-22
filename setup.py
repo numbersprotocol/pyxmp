a49ba268f2e338008e5ba58006e745a807de5560
shc261392 <shc261392@gmail.com>
2020-10-22 16:33:53 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyxmp',
-    version='0.1.0',
+    version='0.1.1',
     description='A Python XMP metadata injection tool based on py3exiv2.',
     url='https://github.com/numbersprotocol/pyxmp',
     author='Numbersprotocol',
